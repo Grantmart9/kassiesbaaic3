ac54258bfcc9a5abaf1029b18e8409347d62d2e0
Grantmart9 <grantmarthinus@gmail.com>
2024-11-01 18:37:47 +0200

    company name

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect, useCallback } from "react";
 import Fade from "@mui/material/Fade";
 import Typography from "@mui/material/Typography";
+import { TopBarButtonStyle } from "components/Display/feutures";
+import Button from "@mui/material/Button";
 import { CardContent, Slide, TextField } from "@mui/material";
 import { Size } from "components/Display/media-query";
 import {
@@ -13,38 +15,48 @@ import {
 } from "components/Display/feutures";
 import Card from "@mui/material/Card";
 
-const topPageMax = 100;
+const image_2 =
+  "url('https://encrypted-tbn2.gstatic.com/licensed-image?q=tbn:ANd9GcT-Iz_bZOducLNnqNF3T2-evxUSne7PaWPBlYZ9XVihpSPAn40hiS-2h05iii1w6YN3saUY28DY7l-TMPruQ5u7ec8wKQzyoCMfT9d9u48')";
 
 const ContactContext = () => {
   return (
     <div className="flex align-center justify-center">
-      <Card sx={{ bgcolor: "#7d9db5" }}>
+      <Card sx={{ bgcolor: "#ebf0f2", opacity: "80%" }}>
         <CardContent>
-          <div className="grid grid-flow-col gap-1">
-            <div>
+          <div className="grid grid-flow-row gap-1 mb-3">
+            <div className="grid grid-flow-col gap-1">
               email:<h1> evandre@telkomsa.co.za</h1>
             </div>
-            <div>
+            <div className="grid grid-flow-col gap-1">
               phone:<h1> +27 82 905 5060</h1>
             </div>
           </div>
           <Typography variant="body1">
             <div className="grid grid-rows-1 gap-1">
-              <div className="text-center justify-center">
-                Name: <TextField size="small" fullWidth="true" />
+              <div className="grid grid-flow-col gap-1">
+                <div className="flex text-center my-auto">Name:</div>
+                <TextField size="small" fullWidth="true" />
               </div>
-              <div className="text-center justify-center">
-                Email: <TextField size="small" fullWidth="true" />
+              <div className="grid grid-flow-col gap-1">
+                <div className="flex text-center my-auto">Email:</div>
+                <TextField size="small" fullWidth="true" />
               </div>
-              <div className="text-center justify-center">
-                Cell: <TextField size="small" fullWidth="true" />
+              <div className="grid grid-flow-col gap-1">
+                <div className="flex text-center my-auto">Cell:</div>
+                <TextField size="small" fullWidth="true" />
               </div>
             </div>
           </Typography>
-          <Card>
-            <div className="text-center justify-center font-bold">Query</div>
-            <TextField fullWidth="true" />
-          </Card>
+          <div className="text-center justify-center font-bold">Query</div>
+          <TextField fullWidth="true" />
+          <div className="flex align-center justify-center mt-3">
+            <Button
+              className="rounded-md"
+              sx={{ backgroundColor: "black", color: "white" }}
+            >
+              Submit
+            </Button>
+          </div>
         </CardContent>
       </Card>
     </div>
@@ -52,94 +64,76 @@ const ContactContext = () => {
 };
 
 export const Contact = ({ topBarOn }) => {
-  var size = Size();
+  const size = Size();
+  const isResponsiveSize = ["XS", "SM", "MD"].includes(size);
   // const [y, setY] = useState(window.scrollY);
 
   // useEffect(() => {
   //   const handleScroll = () => setY(window.scrollY);
   //   window.addEventListener("scroll", handleScroll);
-  //   return () => window.removeEventListener("scroll", handleScroll);
+  //   return () => window.removeEventLitener("scroll", handleScroll);
   // }, []);
 
   useEffect(() => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
   }, []);
 
+  const RenderHeader = () => (
+    <Slide in={true} timeout={1000}>
+      <div>
+        {topBarOn ? (
+          <div>
+            {isResponsiveSize ? (
+              <h1
+                style={{ color: pagetitlecolor, fontFamily: AppFont }}
+                className="text-center justify-center text-3xl mt-32"
+              ></h1>
+            ) : (
+              <h1
+                style={{ color: pagetitlecolor, fontFamily: AppFont }}
+                className="text-center justify-center text-3xl mt-20"
+              ></h1>
+            )}
+          </div>
+        ) : (
+          <div>
+            {isResponsiveSize ? (
+              <h1
+                style={{ color: pagetitlecolor, fontFamily: AppFont }}
+                className="text-center justify-center  text-3xl mt-20"
+              ></h1>
+            ) : (
+              <h1
+                style={{ color: pagetitlecolor, fontFamily: AppFont }}
+                className="text-center justify-center text-3xl mt-20"
+              ></h1>
+            )}
+          </div>
+        )}
+      </div>
+    </Slide>
+  );
+
   return (
-    <div className="grid grid-flow-row gap-1 h-auto">
-      {size == "XS" || size == "SM" || size == "MD" ? (
-        <div>
-          {topBarOn ? (
-            <div className="grid grid-flow-row gap-1 px-2 mt-36">
-              <Slide in={true} timeout={500}>
-                <h1
-                  style={{ color: pagetitlecolor, fontFamily: AppFont }}
-                  className="text-center justify-center font-bold text-3xl"
-                >
-                  Contact Us
-                </h1>
-              </Slide>
-              <Fade in={true} timeout={1500}>
-                <div style={{ width: "100vw" }}>
-                  <ContactContext />
-                </div>
-              </Fade>
-            </div>
-          ) : (
-            <div className="grid grid-flow-row gap-1 px-2 mt-20">
-              <Slide in={true} timeout={500}>
-                <h1
-                  style={{ color: pagetitlecolor, fontFamily: AppFont }}
-                  className="text-center justify-center font-bold text-3xl"
-                >
-                  Contact Us
-                </h1>
-              </Slide>
-              <Fade in={true} timeout={1500}>
-                <div style={{ width: "100vw" }}>
-                  <ContactContext />
-                </div>
-              </Fade>
-            </div>
-          )}
+    <div className="block">
+      <div
+        style={{
+          backgroundImage: image_2,
+          backgroundSize: "cover",
+          opacity: "85%",
+          margin: 0,
+          padding: 0,
+          height: "100vh",
+          position: "sticky",
+          overflow: "scroll",
+          overflowX: "hidden",
+        }}
+      >
+        <RenderHeader />
+        <div className="p-2">
+          <ContactContext />
         </div>
-      ) : (
-        <div>
-          {topBarOn ? (
-            <div className="grid grid-flow-row gap-1 px-2 mt-16">
-              <Fade in={true} timeout={1000}>
-                <h1
-                  style={{ color: pagetitlecolor, fontFamily: AppFont }}
-                  className="text-center justify-center font-bold text-3xl"
-                >
-                  Contact Us
-                </h1>
-              </Fade>
-              <Fade in={true} timeout={1500}>
-                <div style={{ width: "100vw" }}>
-                  <ContactContext />
-                </div>
-              </Fade>
-            </div>
-          ) : (
-            <div className="grid grid-flow-row gap-1 px-2 mt-16">
-              <Fade in={true} timeout={1000}>
-                <h1
-                  style={{ color: pagetitlecolor, fontFamily: AppFont }}
-                  className="text-center justify-center font-bold text-3xl"
-                >
-                  Contact Us
-                </h1>
-              </Fade>
-              <Fade in={true} timeout={1500}>
-                <div style={{ width: "100vw" }}>
-                  <ContactContext />
-                </div>
-              </Fade>
-            </div>
-          )}
-        </div>
-      )}
+      </div>
     </div>
   );
 };
